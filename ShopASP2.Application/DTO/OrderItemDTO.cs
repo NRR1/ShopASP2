2dad23ebb30e07b863b978c3a61878deb83aa9d9
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-ER3FDUM>
2025-03-26 17:19:56 +0300

    .

--- ShopASP2.Application/DTO/OrderItemDTO.cs
+++ ShopASP2.Application/DTO/OrderItemDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopASP2.Application.DTO
+﻿namespace ShopASP2.Application.DTO
 {
     public class OrderItemDTO
     {
