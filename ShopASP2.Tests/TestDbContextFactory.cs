ceccc1e9547c8df32018ab83a58bc9ec0a0e0d02
AvdoshkaMMM <AvdoshkaMMM@DESKTOP-ER3FDUM>
2025-04-07 14:31:29 +0300

    Ошибка компиляции при установке nuget Начало создания тестов

---
+++ ShopASP2.Tests/TestDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopASP2.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopASP2.Tests
+{
+    public static class TestDbContextFactory
+    {
+        public static ShopASP2DBContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ShopASP2DBContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            return new ShopASP2DBContext(options);
+        }
+    }
+}
---
+++ ShopASP2.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ShopASP2.Infrastructure.Repositories;
+
+namespace ShopASP2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task CreateOrderAsync_ShouldCreateOrder_WhenCartIsNotEmpty()
+        {
+            var dbContext = TestDbContextFactory.Create();
+
+            var cartLoggerMock = new Mock<ILogger<CartItemRepository>>();
+            var orderLoggerMock = new Mock<ILogger<OrderRepository>>()
+
+            var cartRepo = new CartItemRepository(dbContext, cartLoggerMock.Object);
+            var orderRepo = new OrderRepository(dbContext, orderLoggerMock.Object);
+
+            int userid = 1;
+        }
+    }
+}
